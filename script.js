4051145eb818546f02fd0b3088684be15406837e
iheb <ihebhosni27@gmail.com>
2023-02-12 10:39:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const cart = {
+    { name: "Item 1", image: "item1.jpg", price: 10, quantity: 1, liked: false },
+    { name: "Item 2", image: "item2.jpg", price: 20, quantity: 1, liked: false },
+    { name: "Item 3", image: "item3.jpg", price: 30, quantity: 1, liked: false },
+    { name: "Item 4", image: "item4.jpg", price: 40, quantity: 1, liked: false },
+    { name: "Item 5", image: "item5.jpg", price: 50, quantity: 1, liked: false },
+    { name: "Item 6", image: "item6.jpg", price: 60, quantity: 1, liked: false },
+    { name: "Item 7", image: "item7.jpg", price: 70, quantity: 1, liked: false },
+    { name: "Item 8", image: "item8.jpg", price: 80, quantity: 1, liked: false },
+    { name: "Item 9", image: "item9.jpg", price: 90, quantity: 1, liked: false },
+    { name: "Item 10", image:
+}
+}
